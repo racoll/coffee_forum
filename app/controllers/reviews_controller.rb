57790bcb4fbcb0bc092691c97f4400f1485268e0
robincollins <racollins1@me.com>
2018-01-11 22:29:02 +0000

    added a show view, and require and permit for security in strong parameters

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,36 @@
 class ReviewsController < ApplicationController
 
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+
   def new
   end
 
   def create
     render plain: params[:review].inspect
+  end
+
+
+# def create
+#   @review = Review.new(params[:review])
+#
+#   @review.save
+#   redirect_to @review
+# end
+
+def create
+  @review = Review.new(review_params)
+
+  @review.save
+  redirect_to @review
+end
+
+private
+  def review_params
+    params.require(:review).permit(:name, :notes)
   end
 
 
