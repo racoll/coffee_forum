d8474a29a5c87709ca8c3cd4c879b408d1fbeead
robincollins <racollins1@me.com>
2018-02-02 18:01:43 +0000

    created a destory method for reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -33,6 +33,13 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+   
+    redirect_to reviews_path
+  end
+
   def update
     @review = Review.find(params[:id])
 
