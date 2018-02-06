04fa9dd674ec250833ab1db6fb6715996816249c
robincollins <racollins1@me.com>
2018-02-06 21:50:21 +0000

    add a destroy action to controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,17 @@
 class CommentsController < ApplicationController
 
   def create
-   @review = Review.find(params[:review_id])
-   @comment = @review.comments.create(comment_params)
-   redirect_to review_path(@review)
- end
+    @review = Review.find(params[:review_id])
+    @comment = @review.comments.create(comment_params)
+    redirect_to review_path(@review)
+  end
+
+  def destroy
+    @review = Review.find(params[:review_id])
+    @comment = @review.comments.find(params[:id])
+    @comment.destroy
+    redirect_to review_path(@review)
+  end
 
  private
    def comment_params
